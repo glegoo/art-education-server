806af1dd464a4b6aed2fb62c7fae8c51a89dffa5
glegoo <chengwei1123@gmail.com>
2019-03-13 10:37:41 +0800

    登录

--- dao/studentSqlMapping.js
+++ dao/studentSqlMapping.js
@@ -1,9 +1,9 @@
 var student = {
-    insert: 'insert into students(id, name, age, sex) values(0,?,?,?,?);',
-    update: 'update students set name=?, age=?, sex=?, add_time=?, where id=?;',
-    delete: 'delete from students where id=?;',
-    queryById: 'select * from students where id=?;',
-    queryAll: 'select * from students;'
-};
+  insert: 'insert into students(id, name, age, sex) values(0,?,?,?,?);',
+  update: 'update students set name=?, age=?, sex=?, add_time=?, where id=?;',
+  delete: 'delete from students where id=?;',
+  queryById: 'select * from students where id=?;',
+  queryAll: 'select * from students;'
+}
 
-module.exports = student;
+module.exports = student
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,33 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 router.post('/login', function (req, res, next) {
-	res.json({
-		code: 200,
-		msg: '登录成功'
-	});
-});
+  res.json({
+    code: 200,
+    data: {
+      token: 'admin'
+    }
+  })
+})
 
-module.exports = router;
+router.get('/info', function (req, res, next) {
+  res.json({
+    code: 200,
+    data: {
+      roles: [
+        'admin'
+      ],
+      name: 'admin',
+      avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
+    }
+  })
+})
+
+router.post('/logout', function (req, res, next) {
+  res.json({
+    code: 200,
+    data: 'success'
+  })
+})
+
+module.exports = router
